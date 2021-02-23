14778291cfd2a06812dfebf6e17b48bc51cd56c7
Younes <68131663+Younes-ops@users.noreply.github.com>
2021-02-23 11:26:54 +0100

    dw

--- src/raideur_triangle.m
+++ src/raideur_triangle.m
@@ -1,13 +1,13 @@
-function M=raideur_triangle(A)
-    % A= [A1(1) , A1(2) ; A2(1) , A2(2) ; A3(1), A3(2) ]
-    alpha=det([ (A(2,1)-A(1,1)) , (A(3,1)-A(1,1)) ; (A(2,2)-A(1,2)) , (A(3,2)-A(1,2)) ]);
+function M=raideur_triangle(T)
+    % T= [T1(1) , T1(2) ; T2(1) , T2(2) ; T3(1), T3(2) ]
+    alpha=det([ (T(2,1)-T(1,1)) , (T(3,1)-T(1,1)) ; (T(2,2)-T(1,2)) , (T(3,2)-T(1,2)) ]);
 
     M=zeros(3,3);
     
     for i=1:3 
         for j=1:3
-            grad_nj=(1/alpha)*[ (A(mod(j+1,3),2)-A(mod(j+2,3),2)) ; (A(mod(j+2,3),1)-A(mod(j+1,3),1))];
-            grad_ni=(1/alpha)*[ (A(mod(i+1,3),2)-A(mod(i+2,3),2)) ; (A(mod(i+2,3),1)-A(mod(i+1,3),1))];
+            grad_nj=(1/alpha)*[ (T(mod(j+1,3),2)-T(mod(j+2,3),2)) ; (T(mod(j+2,3),1)-T(mod(j+1,3),1))];
+            grad_ni=(1/alpha)*[ (T(mod(i+1,3),2)-T(mod(i+2,3),2)) ; (T(mod(i+2,3),1)-T(mod(i+1,3),1))];
             M(i,j)=(alpha/2)*grad_ni'*grad_nj;
         end
     end

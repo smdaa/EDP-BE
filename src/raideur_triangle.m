0344038eb1f841793256df3370669ebf4924a146
unknown <saadmdaa03@gmail.com>
2021-03-02 11:54:38 +0100

    18 agnostic

--- src/raideur_triangle.m
+++ src/raideur_triangle.m
@@ -1,9 +1,7 @@
-function M=raideur_triangle(T)
+function [M,alpha] = raideur_triangle(T)
     % T= [T1(1) , T1(2) ; T2(1) , T2(2) ; T3(1), T3(2) ]
     alpha=det([ (T(2,1)-T(1,1)) , (T(3,1)-T(1,1)) ; (T(2,2)-T(1,2)) , (T(3,2)-T(1,2)) ]);
-
     M=zeros(3,3);
-    
     for i=0:2 
         for j=0:2
             grad_nj=(1/alpha)*[ (T(mod(j+1,3)+1,2)-T(mod(j+2,3)+1,2)) ; (T(mod(j+2,3)+1,1)-T(mod(j+1,3)+1,1))];
@@ -11,5 +9,4 @@ function M=raideur_triangle(T)
             M(i+1,j+1)=(alpha/2)*grad_ni'*grad_nj;
         end
     end
-    
 end

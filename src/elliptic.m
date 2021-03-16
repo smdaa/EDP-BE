ab1d5bf3d6dee17a43705b60e7e8b46a35f4b1c4
unknown <saadmdaa03@gmail.com>
2021-03-16 11:41:52 +0100

    l

--- src/elliptic.m
+++ src/elliptic.m
@@ -2,9 +2,9 @@ clc;
 clear;
 close all;
 %% Initialisation
-n = 6;          % Nombre de points par cote du carré
+n = 4;          % Nombre de points par cote du carré
 Ns = n * n;     % Nombre de points
-[coordinates, elements3, elements4, dirichlet, neumann] = maillage_carre(n);
+[coordinates, elements3, elements4, dirichlet, ~] = maillage_carre(n);
 
 %% Assemblage de la matrice A dans le cas d’un maillage constitue uniquement d'elements triangles;
 A = zeros(Ns);
@@ -24,7 +24,7 @@ end
 for i=1:Ns
        B(i) = B(i) - A(i, dirichlet) * u_d(coordinates(dirichlet, :));
 end
-
+% 
 U = zeros(Ns, 1);
 % condittion de Dirichlet
 U(dirichlet) = u_d(coordinates(dirichlet));
@@ -32,28 +32,37 @@ temp = setdiff(1:Ns, dirichlet);
 U(temp) = A(temp,temp) \ B(temp);
 figure;
 show(elements3, elements4, coordinates, U);
+
 %% Assemblage de la matrice A dans le cas d’un maillage constitue d'elements Quadrangles;
 elements4 = load('elements4.dat');
 neumann = load('neumann.dat');
-A = zeros(Ns);
+coordinates = load('coordinates.dat');
+dirichlet = load('dirichlet.dat');
+
+%A = zeros(Ns);
 for ind = elements4'
-    Qloc = [coordinates(ind(1),:);coordinates(ind(2),:); coordinates(ind(3),:); coordinates(ind(4),:)];
+    Qloc = [coordinates(ind(1),:); coordinates(ind(2),:); coordinates(ind(3),:); coordinates(ind(4),:)];
     [M, alpha] = raideur_quadrangle(Qloc);
     A(ind, ind) = A(ind, ind) + M;
 end
 
 %% Assemblage du second membre dans le cas de conditions de Dirichlet et de Neumann
-B = zeros(Ns, 1);
+%B = zeros(Ns, 1);
 for ind = elements4'
     B(ind) = B(ind) + (alpha / 4) .* f((sum(coordinates(ind, :)) ./ 4));     
 end
 
-% Contribution des conditions de Dirichlet
-for i=1:Ns
-       B(i) = B(i) + A(i, dirichlet) * u_d(coordinates(dirichlet, :));
-end
+% % Contribution des conditions de Dirichlet
+% for i=1:Ns
+%        B(i) = B(i) + A(i, dirichlet) * u_d(coordinates(dirichlet, :));
+% end
 
 % Contribution des conditions de Neumann
+for ind = neumann'  
+   a1 = coordinates(ind(1), :);
+   a2 = coordinates(ind(2), :);
+   B(ind) = B(ind) + (norm(a2 - a1) / 2) * g((a1 + a2) / 2); 
+end
 
 U = zeros(Ns, 1);
 % condittion de Dirichlet
--- src/raideur_quadrangle.m
+++ src/raideur_quadrangle.m
@@ -1,15 +1,18 @@
 function [M, alpha] = raideur_quadrangle(Q)
     alpha = det([(Q(2,1) - Q(1,1)) , (Q(3,1) - Q(1,1)) ; (Q(2,2) - Q(1,2)) , (Q(3,2) - Q(1,2))]);
     Jac_phi = [Q(2, 1) - Q(1, 1), Q(4, 1) - Q(1, 1); Q(2, 2) - Q(1, 2), Q(4, 2) - Q(1, 2)];
-    temp = pinv(Jac_phi' * Jac_phi);
+    temp = Jac_phi' * Jac_phi;
     
-    a = temp(1, 1);
-    b = temp(1, 2);
-    c = temp(2, 1);
+    a = (1 / (temp(1, 1) * temp(2, 2) - temp(1, 2) ^ 2)) * temp(2, 2);
+    b = (-1 / (temp(1, 1) * temp(2, 2) - temp(1, 2) ^ 2)) * temp(1, 2);
+    c = (1 / (temp(1, 1) * temp(2, 2) - temp(1, 2) ^ 2)) * temp(1, 1);
         
     M = (2 * (a + c) + 3 * b) * eye(4);
     M(1:3, 4) = [-2 * c + a; -(a + 3 * b + c); c - 2 * a];
     M(1:2, 3) = [-(a + 3 * b + c); -2 * c + a];
     M(1, 2) = -2 * c + a;
-    M = floor((M + M.') / 2);
+
+    M(2:4, 1) = [-2 * c + a; -(a + 3 * b + c); c - 2 * a];
+    M(3:4, 2) = [c - 2 * a, -(a + 3 * b + c)];
+    M(4, 3) = c - 2 * a;
 end
